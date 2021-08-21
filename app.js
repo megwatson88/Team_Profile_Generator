ae7e8eb3d8dc19ab9846cffd415059da796b0472
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-08-21 12:23:27 -0700

    npm additions

--- app.js
+++ app.js
@@ -80,10 +80,34 @@ function addEmployees(){
         startHtml(newEmployee)
         .then(function(){
             if (moreEmployees === "yes"){
-                addEmployees
+                addEmployees();
             } else {
-                makeHTML
+                makeHTML();
             }
         })
     })
 )};
+
+
+//begin HTML functions 
+function beginHTML(){
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <nav class= "navbar" style = " background-color: slateblue; color: white;" > 
+            <span class = "brandname"> Company Name </span>
+        </nav>
+        <div class = " container"> 
+            <div class="row"> </div>
+            
+        </div>
+    </body>
+    </html>`
+    }
+    fs.writeFile('')
