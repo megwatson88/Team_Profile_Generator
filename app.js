3b6d14487e49762b2fa449f5ddffb9d025f48f76
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-01 21:30:17 -0700

    node fixes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { title } = require('process');
 // varables to store 
-//const Engineer = require()
-//const Intern = require()
-//const Manager = require()
+// This is where the code ref back to the classes created in a different folder. 
+
+// const Engineer = require('./lib/engineer.js')
+// const Intern = require()
+// const Manager = require()
 
 const employees = []
 //function to run node faster
@@ -13,60 +15,85 @@ const employees = []
 //     startHtml();
 //     addMember();
 // };
+
+function generateName(firstName) {
+    return `${firstname}`;
+};
+
+function generateTitle(title) {
+    return `${title}`;
+};
+
+function generateTeamMemberId(idNumber) {
+    return `${idNumber}`
+}
+
 // a function to get the data to populate
-function addEmployees(){
+function addEmployees() {
     inquirer
-    .prompt ({
-        type: 'input',
-        message: 'Enter team member`s name',
-        name: 'name'
-    },
-    {
-        type: 'list',
-        message: 'select team member`s role',
-        name: 'title',
-        choices:[
-            'Engineer',
-            'Intern',
-            'Manager'
-        ]
-    },
-    {
-        type: 'input',
-        message: 'Enter team member`s id',
-        name: 'idNumber'
-    },
-    {
-        type: 'input',
-        message: 'Enter team member`s email address',
-        name: 'email'
-    },
-    {
-        type: 'list',
-        name: 'moreEmployees',
-        message:'Would you like to add more new Employees?',
-        choices:[
-            `Yes`,
-            `No`
-        ]
-    },
-    {
-        type: 'input',
-        message: 'What school do you attend?',
-        name: 'internSchool'
-    },
-    {
-        type: 'input',
-        message: 'What office will this employee have?',
-        name: 'officeNumber'
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub Username?',
-        name: 'githubName'
-    }
- )};
+        .prompt([{
+            type: 'input',
+            message: 'Enter team member`s name',
+            name: 'name'
+        },
+        {
+            type: 'list',
+            message: 'select team member`s role',
+            name: 'title',
+            choices: [
+                'Engineer',
+                'Intern',
+                'Manager'
+            ]
+        },
+        {
+            type: 'input',
+            message: 'Enter team member`s id',
+            name: 'idNumber'
+        },
+        {
+            type: 'input',
+            message: 'Enter team member`s email address',
+            name: 'email'
+        },
+        {
+            type: 'list',
+            name: 'moreEmployees',
+            message: 'Would you like to add more new Employees?',
+            choices: [
+                `Yes`,
+                `No`
+            ]
+        },
+        {
+            type: 'input',
+            message: 'What school do you attend?',
+            name: 'internSchool'
+        },
+        {
+            type: 'input',
+            message: 'What office will this employee have?',
+            name: 'officeNumber'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub Username?',
+            name: 'githubName'
+        }])
+        .then(answers => {
+            console.log(answers)
+            let results = '' 
+            const data = JSON.stringify(answers)
+            fs.writeFile('./Team_Profile_Generator', data,
+                err => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                });
+        })
+}
+addEmployees();
 
-fs.writeFile();
-
-    
+  //write into a file called message.txt, passing in the value of the data variable. if there's an error, the cb function will throw the error; otherwise, prints 'The file has been saved!'
+//work through the classes and test with those classes 
+// ie when you call mangaer add the specific data into an array that cor to manager.
+// work through classes > go through the testing > adding in the HTML though the classes 
