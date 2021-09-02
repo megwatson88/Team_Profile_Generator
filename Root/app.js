4320674c900771f0c25ba6c00e08629a9c306e82
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-02 16:06:54 -0700

    folder moving

--- Root/app.js
+++ Root/app.js
@@ -80,20 +80,53 @@ function addEmployees() {
             message: 'What is your GitHub Username?',
             name: 'githubName'
         }])
-        .then(answers => {
-            console.log(answers)
-            let results = '' 
-            const data = JSON.stringify(answers)
-            fs.writeFile('./Team_Profile_Generator', data,
-                err => {
-                    if (err) throw err;
-                    console.log('The file has been saved!');
-                });
+        .then(function(employeeInfo){
+            let newEmployee;
+            if (title === "Engineer"){
+                newEmployee = new Engineer(name, idNumber, email, title, githubName)
+            } else if (title === "Intern"){
+                newIntern = new Intern(name, idNumber, email, school, githubName)
+            } else if (title === "Manager"){
+                newManger = new Manager (name, idNumber, email, title, officeNumber)
+            }
+    
+            employees.push(newEmployee);
+            startHtml(newEmployee)
+            .then(function(){
+                if (moreEmployees === "yes"){
+                    addEmployees();
+                } else {
+                    makeHTML();
+                }
+            })
         })
-}
+};
 addEmployees();
 
   //write into a file called message.txt, passing in the value of the data variable. if there's an error, the cb function will throw the error; otherwise, prints 'The file has been saved!'
 //work through the classes and test with those classes 
 // ie when you call mangaer add the specific data into an array that cor to manager.
 // work through classes > go through the testing > adding in the HTML though the classes 
+
+    //begin HTML functions 
+function beginHTML(){
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <nav class= "navbar" style = " background-color: slateblue; color: white;" > 
+            <span class = "brandname"> Company Name </span>
+        </nav>
+        <div class = " container"> 
+            <div class="row"> </div>
+            
+        </div>
+    </body>
+    </html>`
+    };
+    beginHTML()
