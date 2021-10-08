21338152d9d7759b10df553802109a64c8908207
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-08 08:55:17 -0500

    manager fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { title } = require('process');
+const { writeFileSync } = require('fs');
 
 // This is where the code ref back to the classes created in a different folder. 
 const Engineer = require('./lib/engineer.js');
@@ -9,6 +10,7 @@ const Intern = require('./lib/intern.js');
 const Manager = require('./lib/manager.js');
 
 const path = require("path");
+const Employee = require('./lib/employee.js');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "index.html"); 
@@ -18,30 +20,35 @@ const employees = []
 const employeeID = []; 
 
 //function to run node faster
-// function initApp(){
-//     startHtml();
-//     addMember();
-// };
-
-// function generateName(firstName) {
-//     return `${firstname}`;
-// };
-
-// function generateTitle(title) {
-//     return `${title}`;
-// };
-
-// function generateTeamMemberId(idNumber) {
-//     return `${idNumber}`
-// }
+function initApp(){
+    startHtml();
+    addMember();
+};
 
 // a function to get the data to populate
+//start with the manager then move to other employees
+function addManager (){
+    console.log(`Let's start with the manager.`);
+    inquirer 
+    //questions to ask: name, id, email, office number, 
+        .prompt([])
+};
 function addEmployees() {
+    console.log(`Let's build your team!`)
     inquirer
         .prompt([{
             type: 'input',
             message: 'Enter team member`s name',
-            name: 'name'
+            name: 'name', 
+            validate: name => {
+                if (name){
+                    return true;
+                } else {
+                    console.log(`Please enter the name`);
+                    return false
+                }
+            }
+        
         },
         {
             type: 'list',
@@ -54,14 +61,22 @@ function addEmployees() {
             ]
         },
         {
-            type: 'input',
+            type: 'number',
             message: 'Enter team member`s id',
-            name: 'idNumber'
+            name: 'idNumber',
+            // make sure the anser is a number 
+            validate: idNumber => {
+                var done = this.async
+                if (typeof(idNumber) ===  "number"){
+                    return true;
+                }
+                return "Please enter an ID Number"
+            }
         },
         {
             type: 'input',
             message: 'Enter team member`s email address',
-            name: 'email'
+            name: 'email',
         },
         {
             type: 'list',
@@ -71,7 +86,10 @@ function addEmployees() {
                 `Yes`,
                 `No`
             ]
-        },
+        },//the code needs to stop and export here 
+        // .then(answer => {
+        //     const pushEmployees
+        // })
         {
             type: 'input',
             message: 'What school do you attend?',
