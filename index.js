3ab7c6f20512a4ff58efa332850a542dc795a20f
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-08 12:03:28 -0500

    tutor help

--- index.js
+++ index.js
@@ -13,129 +13,163 @@ const path = require("path");
 const Employee = require('./lib/employee.js');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "index.html"); 
+const outputPath = path.join(OUTPUT_DIR, "index.html");
 
 
 const employees = []
-const employeeID = []; 
+const employeeID = [];
 
 //function to run node faster
-function initApp(){
-    startHtml();
-    addMember();
-};
+// function initApp() {
+//     startHtml();
+//     addMember();
+// };
 
 // a function to get the data to populate
 //start with the manager then move to other employees
-function addManager (){
-    console.log(`Let's start with the manager.`);
-    inquirer 
-    //questions to ask: name, id, email, office number, 
-        .prompt([])
-};
-function addEmployees() {
-    console.log(`Let's build your team!`)
-    inquirer
-        .prompt([{
-            type: 'input',
-            message: 'Enter team member`s name',
-            name: 'name', 
-            validate: name => {
-                if (name){
-                    return true;
-                } else {
-                    console.log(`Please enter the name`);
-                    return false
+function buildTeam() {
+    function addManager() {
+        console.log(`Let's start with the manager.`);
+        inquirer
+            //questions to ask: name, id, email, office number, 
+            .prompt([{
+                name: "managerName",
+                type: "input",
+                message: "Who is the team manager?"
+            },
+            {
+                name: "managerId",
+                type: "input",
+                message: "What is the manager's ID number?"
+            },
+            {
+                name: "managerEmail",
+                type: "input",
+                message: "What is the manager's email?"
+            },
+            {
+                name: "officeNumber",
+                type: "input",
+                message: "Please enter the managers office number."
+            },
+            ]).then(answers => {
+                const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+                employees.push(manager);
+                //idArray.push(answers.ManagerID);
+                addEmployees();
+            })
+            
+    }; 
+    //addManager();
+    function addEmployees() {
+        console.log(`Let's build your team!`)
+        inquirer
+            .prompt([{
+                type: 'input',
+                message: 'Enter team member`s name',
+                name: 'name',
+                validate: name => {
+                    if (name) {
+                        return true;
+                    } else {
+                        console.log(`Please enter the name`);
+                        return false
+                    }
                 }
-            }
-        
-        },
-        {
-            type: 'list',
-            message: 'select team member`s role',
-            name: 'title',
-            choices: [
-                'Engineer',
-                'Intern',
-                'Manager'
-            ]
-        },
-        {
-            type: 'number',
-            message: 'Enter team member`s id',
-            name: 'idNumber',
-            // make sure the anser is a number 
-            validate: idNumber => {
-                var done = this.async
-                if (typeof(idNumber) ===  "number"){
-                    return true;
+
+            },
+            {
+                type: 'list',
+                message: 'select team member`s role',
+                name: 'title',
+                choices: [
+                    'Engineer',
+                    'Intern'
+                ]
+            },
+            {
+                type: 'number',
+                message: 'Enter team member`s id',
+                name: 'idNumber',
+                // make sure the anser is a number 
+                validate: idNumber => {
+                    var done = this.async
+                    if (typeof (idNumber) === "number") {
+                        return true;
+                    }
+                    return "Please enter an ID Number"
                 }
-                return "Please enter an ID Number"
-            }
-        },
-        {
-            type: 'input',
-            message: 'Enter team member`s email address',
-            name: 'email',
-        },
-        {
-            type: 'list',
-            name: 'moreEmployees',
-            message: 'Would you like to add more new Employees?',
-            choices: [
-                `Yes`,
-                `No`
-            ]
-        },//the code needs to stop and export here 
-        // .then(answer => {
-        //     const pushEmployees
-        // })
-        {
-            type: 'input',
-            message: 'What school do you attend?',
-            name: 'internSchool'
-        },
-        {
-            type: 'input',
-            message: 'What office will this employee have?',
-            name: 'officeNumber'
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub Username?',
-            name: 'githubName'
-        }])
-        .then(function(employeeInfo){
-            let newEmployee;
-            if (title === "Engineer"){
-                newEmployee = new Engineer(name, idNumber, email, title, githubName)
-            } else if (title === "Intern"){
-                newIntern = new Intern(name, idNumber, email, school, githubName)
-            } else if (title === "Manager"){
-                newManger = new Manager (name, idNumber, email, title, officeNumber)
-            }
-    
-            employees.push(newEmployee);
-            startHtml(newEmployee)
-            .then(function(){
-                if (moreEmployees === "yes"){
+            },
+            {
+                type: 'input',
+                message: 'Enter team member`s email address',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'If adding an intern What school did they attend? ',
+                name: 'internSchool'
+            },
+            {
+                type: 'input',
+                message: 'If adding an engineer What is your GitHub Username?',
+                name: 'githubName'
+            }, 
+            {
+                type: 'list',
+                name: 'moreEmployees',
+                message: 'Would you like to add more new Employees?',
+                choices: [
+                    `Yes`,
+                    `No`
+                ]
+            },
+        ])
+            .then(function (employeeInfo) {
+                let newEmployee;
+                if (employeeInfo.title === "Engineer") { //update with employeeInfo. to all the fields
+                    newEmployee = new Engineer(employeeInfo.name, employeeInfo.idNumber, employeeInfo.email, employeeInfo.title, employeeInfo.githubName)
+                } else if (employeeInfo.title === "Intern") {
+                    newIntern = new Intern(employeeInfo.name, employeeInfo.idNumber, employeeInfo.email, employeeInfo.school, employeeInfo.githubName)
+                } 
+
+                employees.push(newEmployee);
+
+                if (employeeInfo.moreEmployees === "yes") {
                     addEmployees();
                 } else {
                     makeHTML();
                 }
+                // startHtml(newEmployee)
+                //     .then(function () {
+                //     })
             })
-        })
-};
-addEmployees();
+    };
+    addManager();
+    //addEmployees();
+}
+    buildTeam();
+function makeHTML(){
+    let HTML = beginHTML() 
+    let content = ""
+    for(let i = 0; i < employees.length; i++){
+        console.log(employees[i])
+     content += "<p>" + employees[i].name + "</p>"
 
-  //write into a file called message.txt, passing in the value of the data variable. if there's an error, the cb function will throw the error; otherwise, prints 'The file has been saved!'
-//work through the classes and test with those classes 
-// ie when you call mangaer add the specific data into an array that cor to manager.
-// work through classes > go through the testing > adding in the HTML though the classes 
+
+    }
+    HTML = HTML.replace("{{body}}", content)
+    console.log(HTML)
+    fs.writeFileSync("employees.html", HTML, "utf-8")
+}
+    //write into a file called message.txt, passing in the value of the data variable. if there's an error, the cb function will throw the error; otherwise, prints 'The file has been saved!'
+    //work through the classes and test with those classes 
+    // ie when you call mangaer add the specific data into an array that cor to manager.
+    // work through classes > go through the testing > adding in the HTML though the classes 
 
     //begin HTML functions 
-function beginHTML(){
-    const html = `<!DOCTYPE html>
+    function beginHTML() {
+        const html = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -148,10 +182,15 @@ function beginHTML(){
             <span class = "brandname"> Company Name </span>
         </nav>
         <div class = " container"> 
-            <div class="row"> </div>
+            <div class="row">  
+            {{body}}
+            </div>
             
         </div>
     </body>
     </html>`
+return html 
+
     };
-    beginHTML()
+
+// beginHTML()
